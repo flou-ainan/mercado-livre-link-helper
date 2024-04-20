aba569c59e7d7f58f6b02161ba8260415f888a32
flou-ainan <flouainan@gmail.com>
2024-04-20 15:57:22 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const notification = document.getElementById("notification")
+input.addEventListener("input", function(event) {
+   event.preventDefault();
+  // remove()
+});
+function notifyCopied(){
+  notification.classList.remove("hidden");
+  notification.classList.remove("reverse-toast-animation");
+  void notification.offsetWidth;
+  notification.classList.add("reverse-toast-animation")
+  // setTimeout(() => notification.classList.add("reverse-toast-animation"), 10)
+}
+function copy() {
+	output.select();
+	document.execCommand('copy');
+  notifyCopied();
+}
+function remove() {
+  let link = input.value
+  link = link.split("?")[0].split("#")[0]
+  //.split("#")[0]
+  output.innerHTML = link
+}
+function clean(){
+  input.value = ""
+}
+
